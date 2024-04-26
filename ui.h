dc5a8691f770b1a23e20641ff4fe3d167f196049
Christine Olsson <96248651+chol2100@users.noreply.github.com>
2024-04-26 17:37:46 +0200

    Update ui.h

--- ui.h
+++ ui.h
@@ -4,20 +4,6 @@
 #include "game.h"
 // Innehåller funktioner för att hantera användargränssnittselement, 
 
-struct Letter {
-	char symbol;
-	sf::Color color;
-
-	Letter() {
-		symbol = ' ';
-		color = sf::Color::White;
-	}
-
-	Letter(char symbol, sf::Color color) {
-		this->symbol = symbol;
-		this->color = color;
-	}
-};
 class Grid {
 	friend class Utilities;
 protected:
@@ -45,7 +31,7 @@ protected:
 	sf::Color lettercolor;
 	std::vector<std::vector<char>>letters;
 	std::vector<std::vector<sf::Color>> letter_colors;
-	
+
 	int count_letters = 0;
 	int current_row = 0;
 
@@ -55,20 +41,21 @@ public:
 	void calc_grid_pos();
 	void add_letters(char letter);
 	void remove_letters();
-	void draw_base(sf::RenderWindow& window, std::vector<char> letter_responses);
+	void draw_base(sf::RenderWindow& window, std::vector<std::string> letter_responses);
 	void reset_count_letters();
-	void update_row_colors(const std::vector<char>& letter_responses);
+	void update_row_colors(const std::vector<std::string>& letter_responses);
 	void game_over_screen(sf::RenderWindow& window, char result, std::string correct_word);
 };
 
 class Keyboard :public Grid {
 private:
-	
+	std::vector<sf::Color> key_colors;
 public:
 	Keyboard();
-	void draw_keyboard(sf::RenderWindow& window, const std::string& currentGuess, const std::vector<char>& letter_responses);
+	void draw_keyboard(sf::RenderWindow& window);
 	char mouse_click(sf::Vector2i mouse_pos, sf::RenderWindow& window);
-	
+	void update_key_colors(const std::vector<std::string>& letter_responses);
+
 };
 
 
