7c5d9b2212f2ad74cc01f52184fc9c7a1c2b78b3
Christine Olsson <96248651+chol2100@users.noreply.github.com>
2024-04-26 17:36:37 +0200

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -36,63 +36,62 @@ void Game::make_guess() {
     letter_responses.clear();
 
     //om user_input är i guessable- ord
-    if (std::find(guessable_words.begin(), guessable_words.end(), user_input) != guessable_words.end() && user_input.length()==5) {
+    if (std::find(guessable_words.begin(), guessable_words.end(), user_input) != guessable_words.end() && user_input.length() == 5) {
         valid_input = true;
         guesses_made++;
         for (int i = 0; i < user_input.length(); i++) {
             if (user_input[i] == correct_word[i]) {
                 std::cout << "Letter correctly placed \n";
-                letter_responses.push_back('C'); // Correct
+                letter_responses.push_back("C" + std::string(1, user_input[i])); // Correct
                 correct_letters_in_word++;
             }
             else if (correct_word.find(user_input[i]) != std::string::npos) {
                 std::cout << "Correct letter but misplaced\n";
-                letter_responses.push_back('M'); // Misplaced
+                letter_responses.push_back("M" + std::string(1, user_input[i])); // Misplaced
             }
             else {
                 std::cout << "No such letter in word \n";
-                letter_responses.push_back('I'); // Incorrect
+                letter_responses.push_back("I" + std::string(1, user_input[i])); // Incorrect
             }
-          
+
         }
         if (correct_letters_in_word == 5) {
             word_guessed = 'T'; // 5 korrekt placerade bokstäver --> True
-            
+
         }
         else {
             word_guessed = 'X';
             correct_letters_in_word = 0;
         }
-        save_guesses = user_input;
+        
         user_input = ""; // Återställ gissningen för nästa försök
     }
-    
+
     else {
         std::cout << user_input << " is not a word";
         valid_input = false;
     }
 
     std::cout << "Your guess was: " << user_input << "\n";
-    
+
 }
 
 // Returnerar de senaste gissningsresultaten
-const std::vector<char>& Game::get_letter_responses() {
+const std::vector<std::string>& Game::get_letter_responses() {
     return letter_responses;
 }
 
 // Huvudloopen för spelet
 void Game::run() {
-    
+
     std::cout << "The secret word is: " << correct_word << "\n";
     sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Wordle-like Game", sf::Style::Fullscreen);
     Grid new_grid;
     Keyboard new_keyboard;
-    
 
     while (window.isOpen()) {
         sf::Event event;
-        
+
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
@@ -112,21 +111,24 @@ void Game::run() {
                 }
                 else if (event.key.code == sf::Keyboard::Enter) {
                     std::cout << "guesses made: " << guesses_made << "\n";
-                    
+
                     // Om rätt ord inte ännu hittats
                     if (word_guessed == 'X' && guesses_made < 6) {
                         make_guess();
                         if (valid_input) {
+                            
                             new_grid.update_row_colors(get_letter_responses()); // Uppdatera färgerna för raden baserat på gissningen
-                            new_grid.reset_count_letters(); // Förbered för nästa rad
+                            new_grid.reset_count_letters();
+                            // Förbered för nästa rad
+                            new_keyboard.update_key_colors(get_letter_responses());
                         }
                     }
-                    if(guesses_made == 6){
+                    if (guesses_made == 6) {
                         // Om word_guessed är: T --> vinst, F --> förlust, X --> spelet pågår fortfarande
                         word_guessed = 'F';
 
                     }
-                    
+
                 }
             }
             else if (event.mouseButton.button == sf::Mouse::Left) {
@@ -140,7 +142,7 @@ void Game::run() {
         }
         window.clear(sf::Color::Black);
         new_grid.draw_base(window, get_letter_responses()); // Rita griden med den senaste gissningen
-        new_keyboard.draw_keyboard(window, save_guesses ,get_letter_responses()); // Rita tangentbordet
+        new_keyboard.draw_keyboard(window); // Rita tangentbordet
         new_grid.game_over_screen(window, word_guessed, correct_word);
         window.display();
     }
